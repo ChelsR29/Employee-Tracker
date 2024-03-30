fcfd3d6d764a5bdb3559b4aebd6a35e88e5106c2
Chelsea <chibby@chelseas-mbp.mynetworksettings.com>
2024-03-30 13:55:54 -0400

    Add CLI.js File/Code

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,15 @@
-DROP DATABASE IF EXISTS employee_db:
+DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
 
 CREATE TABLE department (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(30)
 );
 
 CREATE TABLE role (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(30),
   salary DECIMAL(10, 2),
   department_id INT,
@@ -17,7 +17,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
@@ -25,3 +25,4 @@ CREATE TABLE employee (
   FOREIGN KEY (role_id) REFERENCES role (id),
   FOREIGN KEY (manager_id) REFERENCES employee (id)
 );
+
